a87582d46a0e457f8372795f4879fd763881fc31
Ryan Perneel <perneel@gmail.com>
2020-01-09 19:47:29 -0500

    fixed typo :)

--- powershell/forceKillProcesses.ps1
+++ powershell/forceKillProcesses.ps1
@@ -2,7 +2,7 @@ $services = Get-Service "SOMESTRING*" | Where-Object { $_.Status -eq "Running" }
 if ($services) {
   foreach ($service in $services) {
     # kill the process for this service
-    $id = Get-WmiObject -Class win32_class -Filter "name = '$($service.Name)'"
+    $id = Get-WmiObject -Class win32_service -Filter "name = '$($service.Name)'"
     Write-Output "Killing PID $id for $($service.Name)"
     Stop-Process -Id $id -Force
   }
